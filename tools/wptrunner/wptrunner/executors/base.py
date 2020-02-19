7583829111aade3e1eade0f58b7960139772e9f7
James Graham <james@hoppipolla.co.uk>
2020-02-19 11:15:23 +0000

    Pass logger explicitly in to Executors
    This means that we are able to report errors which happen during setup

--- tools/wptrunner/wptrunner/executors/base.py
+++ tools/wptrunner/wptrunner/executors/base.py
@@ -211,8 +211,9 @@ class TestExecutor(object):
     extra_timeout = 5  # seconds
 
 
-    def __init__(self, browser, server_config, timeout_multiplier=1,
+    def __init__(self, logger, browser, server_config, timeout_multiplier=1,
                  debug_info=None, **kwargs):
+        self.logger = logger
         self.runner = None
         self.browser = browser
         self.server_config = server_config
@@ -221,12 +222,6 @@ class TestExecutor(object):
         self.last_environment = {"protocol": "http",
                                  "prefs": {}}
         self.protocol = None  # This must be set in subclasses
-
-    @property
-    def logger(self):
-        """StructuredLogger for this executor"""
-        if self.runner is not None:
-            return self.runner.logger
 
     def setup(self, runner):
         """Run steps needed before tests can be started e.g. connecting to
@@ -312,9 +307,9 @@ class TestharnessExecutor(TestExecutor):
 class RefTestExecutor(TestExecutor):
     convert_result = reftest_result_converter
 
-    def __init__(self, browser, server_config, timeout_multiplier=1, screenshot_cache=None,
+    def __init__(self, logger, browser, server_config, timeout_multiplier=1, screenshot_cache=None,
                  debug_info=None, **kwargs):
-        TestExecutor.__init__(self, browser, server_config,
+        TestExecutor.__init__(self, logger, browser, server_config,
                               timeout_multiplier=timeout_multiplier,
                               debug_info=debug_info)
 
@@ -498,11 +493,11 @@ class WdspecExecutor(TestExecutor):
     convert_result = pytest_result_converter
     protocol_cls = None
 
-    def __init__(self, browser, server_config, webdriver_binary,
+    def __init__(self, logger, browser, server_config, webdriver_binary,
                  webdriver_args, timeout_multiplier=1, capabilities=None,
                  debug_info=None, **kwargs):
         self.do_delayed_imports()
-        TestExecutor.__init__(self, browser, server_config,
+        TestExecutor.__init__(self, logger, browser, server_config,
                               timeout_multiplier=timeout_multiplier,
                               debug_info=debug_info)
         self.webdriver_binary = webdriver_binary
