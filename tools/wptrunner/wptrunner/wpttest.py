c8c51e7e0b90b3baa5db7b90d9b1f45817dd1512
James Graham <james@hoppipolla.co.uk>
2020-02-20 15:12:42 +0000

    Add basic print reftest support to marionette executor

--- tools/wptrunner/wptrunner/wpttest.py
+++ tools/wptrunner/wptrunner/wpttest.py
@@ -7,7 +7,7 @@ from six import iteritems, string_types
 from .wptmanifest.parser import atoms
 
 atom_reset = atoms["Reset"]
-enabled_tests = {"testharness", "reftest", "wdspec", "crashtest"}
+enabled_tests = {"testharness", "reftest", "wdspec", "crashtest", "print-reftest"}
 
 
 class Result(object):
@@ -442,7 +442,7 @@ class ReftestTest(Test):
                 raise ValueError
 
         self.references = references
-        self.viewport_size = viewport_size
+        self.viewport_size = self.get_viewport_size(viewport_size)
         self.dpi = dpi
         self._fuzzy = fuzzy or {}
 
@@ -533,6 +533,10 @@ class ReftestTest(Test):
             reference.update_metadata(metadata)
         return metadata
 
+    def get_viewport_size(self, override):
+        assert override is None
+        return None
+
     @property
     def id(self):
         return self.url
@@ -568,6 +572,13 @@ class ReftestTest(Test):
         return values
 
 
+class PrintReftestTest(ReftestTest):
+    test_type = "print-reftest"
+
+    def get_viewport_size(self, override):
+        assert override is None
+        return (5*2.54, 3*2.54)
+
 class WdspecTest(Test):
     result_cls = WdspecResult
     subtest_result_cls = WdspecSubtestResult
@@ -583,6 +594,7 @@ class CrashTest(Test):
 
 
 manifest_test_cls = {"reftest": ReftestTest,
+                     "print-reftest": PrintReftestTest,
                      "testharness": TestharnessTest,
                      "manual": ManualTest,
                      "wdspec": WdspecTest,
